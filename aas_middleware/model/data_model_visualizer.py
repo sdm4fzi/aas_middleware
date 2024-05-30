5ea88f794a966a287a9fad92fad91fa7bcad7201
Sebastian Behrendt <sebastianbehrendt97@gmail.com>
2024-05-30 20:03:26 +0200

    updated visualization and saving of graph kpis

--- aas_middleware/model/data_model_visualizer.py
+++ aas_middleware/model/data_model_visualizer.py
@@ -1,5 +1,4 @@
-
-
+from typing import Dict, Literal, Optional
 import igraph as ig
 from matplotlib import pyplot as plt
 from pydantic import BaseModel
@@ -8,14 +7,22 @@ from aas_middleware.model.reference_finder import ReferenceType
 from aas_middleware.model.util import get_id_with_patch
 
 
-def get_instance_graph(data_model: DataModel, name: str="Data Model Type Graph") -> ig.Graph:
+def get_instance_graph(
+    data_model: DataModel, name: str = "Data Model Type Graph"
+) -> ig.Graph:
     unique_names = set()
     for schema in data_model._reference_infos:
         unique_names.add(schema.identifiable_id)
         unique_names.add(schema.reference_id)
     n_vertices = len(unique_names)
     model_id_map = {model_id: counter for counter, model_id in enumerate(unique_names)}
-    edges = [(model_id_map[reference_info.identifiable_id], model_id_map[reference_info.reference_id]) for reference_info in data_model._reference_infos]
+    edges = [
+        (
+            model_id_map[reference_info.identifiable_id],
+            model_id_map[reference_info.reference_id],
+        )
+        for reference_info in data_model._reference_infos
+    ]
     graph = ig.Graph(n_vertices, edges, directed=True)
     graph["title"] = name
     graph.vs["name"] = list(unique_names)
@@ -32,18 +39,28 @@ def get_instance_graph(data_model: DataModel, name: str="Data Model Type Graph")
         else:
             node_types.append("Primitive")
     graph.vs["type"] = node_types
-    graph.es["type"] = [reference_info.reference_type for reference_info in data_model._reference_infos]
+    graph.es["type"] = [
+        reference_info.reference_type for reference_info in data_model._reference_infos
+    ]
     return graph
 
 
-def get_type_graph(data_model: DataModel, name: str="Data Model Instance Graph") -> ig.Graph:
+def get_type_graph(
+    data_model: DataModel, name: str = "Data Model Instance Graph"
+) -> ig.Graph:
     unique_names = set()
     for schema in data_model._schema_reference_infos:
         unique_names.add(schema.identifiable_id)
         unique_names.add(schema.reference_id)
     n_vertices = len(unique_names)
     schema_id_map = {model_id: counter for counter, model_id in enumerate(unique_names)}
-    edges = [(schema_id_map[reference_info.identifiable_id], schema_id_map[reference_info.reference_id]) for reference_info in data_model._schema_reference_infos]
+    edges = [
+        (
+            schema_id_map[reference_info.identifiable_id],
+            schema_id_map[reference_info.reference_id],
+        )
+        for reference_info in data_model._schema_reference_infos
+    ]
     graph = ig.Graph(n_vertices, edges, directed=True)
     graph["title"] = name
     graph.vs["name"] = list(unique_names)
@@ -56,14 +73,17 @@ def get_type_graph(data_model: DataModel, name: str="Data Model Instance Graph")
         else:
             node_types.append("Primitive")
     graph.vs["type"] = node_types
-    graph.es["type"] = [reference_info.reference_type for reference_info in data_model._schema_reference_infos]
+    graph.es["type"] = [
+        reference_info.reference_type
+        for reference_info in data_model._schema_reference_infos
+    ]
     return graph
 
-def visualize_graph(graph: ig.Graph):
+
+def visualize_graph(graph: ig.Graph, show: bool = False, save: Optional[Literal["png", "jpg", "svg"]] = None):
 
     # graph.vs["name"] = ["\n\n" + label.split(".")[-1] for label in graph.vs["name"]]
     graph.vs["name"] = [label.split(".")[-1] for label in graph.vs["name"]]
-
 
     edge_colors = []
     for edge in graph.es["type"]:
@@ -85,42 +105,92 @@ def visualize_graph(graph: ig.Graph):
             node_labels.append(node["name"])
         elif node["type"] == "Primitive":
             node_colors.append("#ced4da")
-            node_labels.append("")
-            # node_labels.append(node["name"])
+            # node_labels.append("")
+            node_labels.append(node["name"])
 
     fig, ax = plt.subplots()
 
-    ig.plot(graph,
-            target=ax,
-            # layout="circle",
-            vertex_color=node_colors,
-            vertex_frame_width=0.0,
-            vertex_size=10,
-            vertex_label_size=7.0,
-            vertex_label=node_labels,
-            edge_width=1,
-            edge_color=edge_colors,
+    # _layout_mapping = {
+    #     "auto": "layout_auto",
+    #     "automatic": "layout_auto",
+    #     "bipartite": "layout_bipartite",
+    #     "circle": "layout_circle",
+    #     "circular": "layout_circle",
+    #     "davidson_harel": "layout_davidson_harel",
+    #     "dh": "layout_davidson_harel",
+    #     "drl": "layout_drl",
+    #     "fr": "layout_fruchterman_reingold",
+    #     "fruchterman_reingold": "layout_fruchterman_reingold",
+    #     "graphopt": "layout_graphopt",
+    #     "grid": "layout_grid",
+    #     "kk": "layout_kamada_kawai",
+    #     "kamada_kawai": "layout_kamada_kawai",
+    #     "lgl": "layout_lgl",
+    #     "large": "layout_lgl",
+    #     "large_graph": "layout_lgl",
+    #     "mds": "layout_mds",
+    #     "random": "layout_random",
+    #     "rt": "layout_reingold_tilford",
+    #     "tree": "layout_reingold_tilford",
+    #     "reingold_tilford": "layout_reingold_tilford",
+    #     "rt_circular": "layout_reingold_tilford_circular",
+    #     "reingold_tilford_circular": "layout_reingold_tilford_circular",
+    #     "sphere": "layout_sphere",
+    #     "spherical": "layout_sphere",
+    #     "star": "layout_star",
+    #     "sugiyama": "layout_sugiyama",
+    # }
+
+    ig.plot(
+        graph,
+        target=ax,
+        # layout="circle",
+        layout="kk",
+        vertex_color=node_colors,
+        vertex_frame_width=0.0,
+        vertex_size=10,
+        vertex_label_size=7.0,
+        vertex_label=node_labels,
+        edge_width=1,
+        edge_color=edge_colors,
     )
     fig.set_size_inches(20, 20)
-    plt.show()
+    if show:
+        plt.show()
+    # plt.show()
     # Save the graph as an image file
-    # fig.savefig('social_network.png')
-    # fig.savefig('social_network.jpg')
-    # fig.savefig('social_network.pdf')
+    if save == "png":
+        fig.savefig(f"{graph["title"]}.png")
+    elif save == "jpg":
+        fig.savefig(f"{graph["title"]}.jpg")
+    elif save == "svg":
+        fig.savefig(f"{graph["title"]}.svg")
+
+import numpy as np
 
 
 class GraphMetrics(BaseModel):
+    name: str
     number_of_vertices: int
     number_of_edges: int
     density: float
-    diameter: int
-    average_path_length: float
+    in_degree_mean: float
+    in_degree_std: float
+    out_degree_mean: float
+    out_degree_std: float
+
 
 def calculate_graph_metrics(graph: ig.Graph) -> GraphMetrics:
+    indegree = np.array(graph.indegree())
+    outdegree = np.array(graph.outdegree())
+
     return GraphMetrics(
+        name=graph["title"],
         number_of_vertices=graph.vcount(),
         number_of_edges=graph.ecount(),
         density=graph.density(),
-        diameter=graph.diameter(),
-        average_path_length=graph.average_path_length()
+        in_degree_mean=np.mean(indegree),
+        in_degree_std=np.std(indegree),
+        out_degree_mean=np.mean(outdegree),
+        out_degree_std=np.std(outdegree),
     )
